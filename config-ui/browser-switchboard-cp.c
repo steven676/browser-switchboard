349339b659df6d47a45424689a20e8a5d9610ddf
Steven Luo <steven+maemo@steven676.net>
2009-12-17 00:02:44 -0800

    Remove maemo.org contact address from copyright notice
    The maemo/Nokia folks aren't responsible for this code in any way, so
    don't give that impression.

--- config-ui/browser-switchboard-cp.c
+++ config-ui/browser-switchboard-cp.c
@@ -7,7 +7,6 @@
  * Derived from services-cp.c from maemo-control-services
  * Copyright (c) 2008 Janne Kataja <janne.kataja@iki.fi>
  * Copyright (c) 2008 Nokia Corporation
- * Contact: integration@maemo.org
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

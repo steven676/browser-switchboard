2d7adcf36ddb123a5b4a147df08f48c0afc42d7b
Steven Luo <steven+maemo@steven676.net>
2009-12-17 04:14:27 -0800

    Don't explicitly set a size for the dialog

--- config-ui/browser-switchboard-cp.c
+++ config-ui/browser-switchboard-cp.c
@@ -285,7 +285,8 @@ static GtkDialog *swb_config_dialog(void) {
 	GtkWidget *okbutton, *cancelbutton;
 
 	dialog = gtk_dialog_new();
-	gtk_widget_set_size_request(GTK_WIDGET(dialog), 580, 240);
+	/* Doesn't seem to be necessary?
+	   gtk_widget_set_size_request(GTK_WIDGET(dialog), 580, 180); */
 	gtk_window_set_title (GTK_WINDOW(dialog), "Browser Switchboard");
 	gtk_window_set_type_hint (GTK_WINDOW(dialog), GDK_WINDOW_TYPE_HINT_DIALOG);
 

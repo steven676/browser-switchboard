870b63f0602922c0f135d3982f1febfe2706c243
Steven Luo <steven+maemo@steven676.net>
2010-02-14 17:28:54 -0800

    Install the inotify watch for lockfile creation before forking
    This avoids the silliness of doing this in both the parent and child
    process.

--- launcher.c
+++ launcher.c
@@ -204,10 +204,6 @@ void launch_microb(struct swb_context *ctx, char *uri) {
 	/* Release the osso_browser D-Bus name so that MicroB can take it */
 	dbus_release_osso_browser_name(ctx);
 
-	if ((pid = fork()) == -1) {
-		perror("fork");
-		exit(1);
-	}
 #ifdef FREMANTLE
 	/* Put together the path to the MicroB browserd lockfile */
 	if (!(homedir = getenv("HOME")))
@@ -242,6 +238,11 @@ void launch_microb(struct swb_context *ctx, char *uri) {
 		exit(1);
 	}
 	free(microb_profile_dir);
+
+	if ((pid = fork()) == -1) {
+		perror("fork");
+		exit(1);
+	}
 
 	if (pid > 0) {
 		/* Parent process */
@@ -477,6 +478,11 @@ void launch_microb(struct swb_context *ctx, char *uri) {
 		execl("/usr/bin/maemo-invoker", "browser", (char *)NULL);
 	}
 #else /* !FREMANTLE */
+	if ((pid = fork()) == -1) {
+		perror("fork");
+		exit(1);
+	}
+
 	if (pid > 0) {
 		/* Parent process */
 		waitpid(pid, &status, 0);
